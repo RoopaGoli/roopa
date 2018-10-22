51be9e042b604379a8143c709687380563cae805
RoopaGoli <44366716+RoopaGoli@users.noreply.github.com>
2018-10-23 00:39:58 +0530

    Add files via upload

---
+++ MemberDetailsMock.java
@@ -0,0 +1,17 @@
+package com.hackathon.service.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to run MemberDetailsMock as spring boot component
+ */
+@SpringBootApplication
+public class MemberDetailsMock {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MemberDetailsMock.class, args);
+
+	}
+}
